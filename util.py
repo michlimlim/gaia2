569a0179848144b17d2189b285b9e21a948efa54
George Saussy <georgesaussy@google.com>
2019-04-29 21:15:10 -0400

    Document code in util.py

--- util.py
+++ util.py
@@ -1,7 +1,9 @@
-# TODO(gs): Document this file.
-
 class EmptyQueueError(Exception):
+    # EmptyQueueError is raised when a dequeue is run
+    # on empty queues.
     pass
 
 class DevicePushbackError(Exception):
+    # DevicePushbackError is raised when a device cannot
+    # enqueue another update.
     pass
