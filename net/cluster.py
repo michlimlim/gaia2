649f0437b47126e5be17413fa93e895cd101a914
Yuki <yukidepourbaix@gmail.com>
2019-05-06 16:07:06 -0400

    modified:   net/cluster.py 	modified:   net/cluster_test.py

--- net/cluster.py
+++ net/cluster.py
@@ -1,14 +1,77 @@
 import random
 
+ch = {}
+
 class Node(object):
 
     def __init__(self):
         self.ID = id(self)
         self.weight = random.randint(1, 10)
         self.cluster = []
+        self.clusterHead = None
+        self.neighbours = []
+        ch[self] = False
+        self.start()
 
-    def addCluster(self, n):
-        self.cluster.append(n)
+    def start(self):
+        big = self.largestNeighbour()
+        if big and big.weight > self.weight:
+            self.callJoin(big)
+            self.clusterHead = big
+        else:
+            self.callCh()
+            ch[self] = True
+            self.clusterHead = self
+            if self not in self.cluster:
+                self.cluster.append(self)
 
     def show(self):
-        print("ID = %s | Weight = %s | cluster = %s" % (self.ID, self.weight, self.cluster))
+        print("ID = %s | Weight = %s | clusterhead = %s" %
+              (self.ID, self.weight, self.clusterHead.ID))
+        print(" ".join([str(mem.ID) for mem in self.cluster]))
+
+    def largestNeighbour(self):
+        max = 0
+        biggest = None
+        for neighbour in self.neighbours:
+            if neighbour.weight > max and ch(neighbour):
+                max = neighbour.weight
+                biggest = neighbour
+        return biggest
+
+    def callJoin(self, big):
+        for neighbour in self.neighbours:
+            neighbour.getJoin(self, big)
+
+    def getJoin(self, u, z):
+        if ch[self]:
+            if z == self:
+                self.cluster.append(u)
+            elif u in self.cluster:
+              self.cluster.remove(u)
+        elif self.clusterHead == u:
+            self.start()
+
+    def callCh(self):
+        for neighbour in self.neighbours:
+            neighbour.getCh(self)
+
+    def getCh(self, u):
+        if not self.clusterHead or u.weight > self.clusterHead.weight:
+            self.callJoin(u)
+            ch[self] = False
+
+    def newLink(self, u):
+        self.neighbours.append(u)
+        if ch[u]:
+            if self.clusterHead and u.weight > self.clusterHead.weight:
+                self.callJoin(u)
+                self.clusterHead = u
+                ch[self] = False
+
+    def linkFail(self, u):
+        self.neighbours.remove(u)
+        if ch[self] and u in self.cluster:
+            self.cluster.remove(u)
+        elif self.clusterHead == u:
+            self.start()
--- net/cluster_test.py
+++ net/cluster_test.py
@@ -1,11 +1,30 @@
 from cluster import Node
+from cluster import ch
+
+def createEdge(a, b):
+    a.newLink(b)
+    b.newLink(a)
+
+def breakEdge(a, b):
+    if b in a.neighbours:
+        a.linkFail(b)
+        b.linkFail(a)
+
+def nodeFail(a):
+    for neighbour in a.neighbours:
+        breakEdge(a, neighbour)
 
 if __name__ == "__main__":
-    a = Node()
-    b = Node()
-    c = Node()
-    a.addCluster(b)
-    a.addCluster(c)
-    a.show()
-    a.cluster[0].show()
-    a.cluster[1].show()
+    arr  = []
+    for i in range(0, 5):
+        arr.append(Node())
+
+    createEdge(arr[0], arr[1])
+
+    arr[0].show()
+    arr[1].show()
+
+    breakEdge(arr[0], arr[1])
+
+    arr[0].show()
+    arr[1].show()
